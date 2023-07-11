a2a25ab2a2803002fe0b23220ebb35eade2e12cd
Fatima--Salim <salimfatima445@gmail.com>
2023-07-11 15:18:32 -0700

    task

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,33 +1,20 @@
 #!/usr/bin/python3
-'Log Parsing'
-import sys
+"""Defines a text file insertion function."""
 
-status_list = [200, 301, 400, 401, 403, 404, 405, 500]
-try:
-    total_size = 0
-    final_list = []
-    for index, line in enumerate(sys.stdin, 1):
-        if line:
-            line_split = line.split()
-            if len(line_split) > 2:
-                if line_split[-1].isnumeric() and line_split[-2].isnumeric():
-                    size = line_split[-1]
-                    status = line_split[-2]
-                    total_size += int(size)
-            if len(status) > 0 and int(status) in status_list:
-                final_list.append(int(status))
-        if index % 10 == 0:
-            print('File size: {}'.format(total_size))
-            for i in status_list:
-                if i in final_list:
-                    status_count = final_list.count(i)
-                    print("{}: {}".format(i, status_count))
-except Exception:
-    pass
 
-finally:
-    print('File size: {}'.format(total_size))
-    for i in status_list:
-        if i in final_list:
-            status_count = final_list.count(i)
-            print("{}: {}".format(i, status_count))
+def append_after(filename="", search_string="", new_string=""):
+    """Insert text after each line containing a given string in a file.
+
+    Args:
+        filename (str): The name of the file.
+        search_string (str): The string to search for within the file.
+        new_string (str): The string to insert.
+    """
+    text = ""
+    with open(filename) as r:
+        for line in r:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as w:
+        w.write(text)
